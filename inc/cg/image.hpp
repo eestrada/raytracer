62659156a80c5c866c230936fe3e706e3b129441
Ethan Estrada <ethan.estrada@gmail.com>
2013-12-10 20:07:50 -0700

    cs455/project05. Implement Image class and PPM writer function

--- inc/cg/image.hpp
+++ inc/cg/image.hpp
@@ -2,7 +2,10 @@
 #define IMAGE_HPP
 
 #include <vector>
+#include <string>
+#include <ostream>
 #include <memory>
+#include <cstdint>
 
 #include "cg_utils.hpp"
 #include "Vec4.hpp"
@@ -11,19 +14,64 @@
 namespace cg
 {
 
-namespace image
-{
-
 template <typename T>
-struct pixel
+struct pixel_type
 {
     T r, g, b, a;
+
+    typedef T chan_type;
 };
 
-typedef pixel<uint8_t> pixelb;
-typedef pixel<uint16_t> pixelh;
-typedef pixel<uint32_t> pixeli;
-typedef pixel<float> pixelf;
+template <typename T>
+pixel_type<T> pixel_ctor(T r,T g,T b,T a)
+{
+    pixel_type<T> retval;
+
+    retval.r = r;
+    retval.g = g;
+    retval.b = b;
+    retval.a = a;
+
+    return retval;
+}
+
+typedef pixel_type<uint8_t> pixelb;
+typedef pixel_type<uint16_t> pixelh;
+typedef pixel_type<uint32_t> pixeli;
+typedef pixel_type<float> pixelf;
+
+/*
+ * Image class
+ *
+ * Scanline storage starting at top-left corner.
+ */
+struct Image
+{
+public: //functions
+    Image(uint16_t w=0, uint16_t h=0);
+
+    void set_dimensions(uint16_t w, uint16_t h);
+    uint16_t get_width() const;
+    uint16_t get_height() const;
+    uint32_t size() const;
+
+    // index into the one dimensional array
+    const pixelf & operator[](uint32_t index) const;
+    pixelf & operator[](uint32_t index);
+
+    // index into the image using the x and y coordinates
+    const pixelf & at(uint16_t x, uint16_t y) const;
+    pixelf & at(uint16_t x, uint16_t y);
+
+    // Nearest pixel interpolation. In other words, point filtering.
+    const pixelf & at_uv(double u, double v) const;
+    pixelf & at_uv(double u, double v);
+
+protected: //variables
+    uint16_t width, height;
+    std::vector<pixelf> pixels;
+};
+
 
 struct bitmap
 {
@@ -33,14 +81,10 @@ struct bitmap
     const uint8_t *raw() const;
 };
 
-////////////////////////////////////////////////////////////
-// FUNCTION LoadPPM(char *)
-// Load a binary ppm file into a bitmap and return it
-////////////////////////////////////////////////////////////
+pixelb float_to_byte(const pixelf &pf, bool premult=true);
 
-bitmap loadPPM(const std::string &filename);
+bool writePPM(const Image &img, std::ostream &out, bool premult=true);
 
-} //end namespace image
 } //end namespace cg
 
 #endif // end include guard
