a432940f29b4a6f4b933864b96541011d5f31682
Ethan Estrada <ethan.estrada@gmail.com>
2013-12-12 19:07:21 -0700

    cs455/project05. Improve lights and scene management

--- inc/cg/Object.hpp
+++ inc/cg/Object.hpp
@@ -2,6 +2,7 @@
 #define OBJECT_HPP
 #include <memory>
 #include <vector>
+#include <ostream>
 
 #include "Vec3.hpp"
 #include "rt/Ray.hpp"
@@ -27,7 +28,6 @@ public: // Variables
     std::shared_ptr<object> parent;
 };
 
-
 struct geo : public object
 {
 public:
@@ -39,7 +39,8 @@ public:
 
 struct camera : public object
 {
-    double fovy, aspect, znear, zfar;
+    uint16_t xres, yres;
+    double fov, aspect, znear, zfar;
     cg::Vec3 lookat, lookfrom, lookup;
 };
 
@@ -53,28 +54,47 @@ typedef light amb_lght;
 
 struct dist_lght : public light
 {
+    virtual cg::Clr3 contribution(const cg::Vec3 &position);
     cg::Vec3 lookat;
 };
 
 struct pt_lght : public light
 {
+    virtual cg::Clr3 contribution(const cg::Vec3 &position);
     cg::Vec3 pos;
 };
 
 struct spot_lght : public pt_lght
 {
+    virtual cg::Clr3 contribution(const cg::Vec3 &position);
     cg::Vec3 dir;
     double cone_angle, penumbra;
 };
 
 typedef object null;
 
+struct scene
+{
 std::vector< std::shared_ptr<geo> > scene_geo;
 
 std::vector< std::shared_ptr<light> > scene_lights;
 
-std::shared_ptr<object> scene_camera;
+std::shared_ptr<camera> scene_camera;
+};
+
 } // end obj namespace
+
+inline std::ostream & operator<<(std::ostream &out, const obj::camera &cam)
+{
+    out << "Camera settings:\n";
+    out << "Resolution " << cam.xres << "x" << cam.yres << "\n";
+    out << "Field of View " << cam.fov << "\n";
+    out << "Look At " << cam.lookat << "\n";
+    out << "Look From " << cam.lookfrom << "\n";
+    out << "Look Up " << cam.lookup << "\n";
+
+    return out;
+}
 
 #endif //finish include guard
 
--- src/Object.cpp
+++ src/Object.cpp
@@ -11,14 +11,25 @@ cg::Mat4 object::get_xform() const
 {
     if (parent != NULL)
     {
-        cg::Mat4 pxform = parent->get_xform();
         // Do child transform THEN parent to get expected result
-        return pxform * transform;
+        return parent->get_xform() * transform;
     }
     else
     {
         return transform;
     }
+}
+
+cg::Clr3 light::contribution(const cg::Vec3 &position)
+{
+    // Just return the light color. Ambient lights are so easy. :)
+    return lgt_clr;
+}
+
+cg::Clr3 dist_lght::contribution(const cg::Vec3 &position)
+{
+    // Lazily return our parent's implementation for now.
+    return light::contribution(position);
 }
 
 rt::RayHit geo::trace(const rt::Ray &ray)
